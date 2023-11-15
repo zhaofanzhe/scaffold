f72ae50f98310f09998602810418dc0a13a16a9d
arthur <actar676309180@gmail.com>
2023-11-15 15:05:49 +0800

    本地存储配置

--- storage-local/src/main/java/com/github/zhaofanzhe/scaffold/storage/LocalStorageConfig.java
+++ storage-local/src/main/java/com/github/zhaofanzhe/scaffold/storage/LocalStorageConfig.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.io.File;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,4 +23,15 @@ public class LocalStorageConfig {
      */
     private String baseUrl;
 
+    public void setPath(String path) {
+        // 如果不是绝对路径, 转换为绝对路径
+        // MultipartFile.transferTo(File) 的特性, 不是绝对路径会拼接 MultipartFile 的路径
+        // By the way, MultipartFile.transferTo(Path) 没有这个特性
+        // 因此需要转换为绝对路径
+        final File file = new File(path);
+        if (!file.isAbsolute()) {
+            path = file.getAbsolutePath();
+        }
+        this.path = path;
+    }
 }
